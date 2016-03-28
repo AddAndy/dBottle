9f218bc8ca6c592694e09b28a72f4c92568454fe
Andrew Taylor <andrew@andrew-taylor.ca>
2016-03-27 18:03:10 -0600

    modified gui, now works in a passive thread. added sigterm (ctrlC to exit), add a buffer to print things to the screen

---
+++ gui.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import random
+from threading import Thread
+import Queue
+from PyQt4.QtGui import *
+from PyQt4 import QtCore
+
+class LEDFakeout(QTableWidget):
+
+    def __init__(self,frameBuffer):
+        self.frameBuffer = frameBuffer;
+        QTableWidget.__init__(self)
+        self.initUI()
+
+    def initUI(self):
+        self.resize(640,1080)
+        self.setWindowTitle("dbottle")
+        self.setRowCount(30);
+        self.setColumnCount(5);
+
+        for c in range(0,5):
+            for r in range(0,30):
+                self.setItem(r,c,QTableWidgetItem("{},{}".format(r,c)))
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.updateBackground)
+        self.timer.start(100)
+        self.show()
+
+    def updateBackground(self):
+        frame = self.frameBuffer.get();
+
+        for c in range(0,5):
+            for r in range(0,30):
+                tableItem = self.item(r,c);
+                (R,G,B,Bri) = frame.getPixel(r,c).toQuad();
+                tableItem.setBackground(QColor(R,G,B))
+
+
+if __name__ == '__main__':
+    print "Error, please run dBottle.py"
